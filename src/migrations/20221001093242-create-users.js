c49bcc43824e7aa97d56782106f82a8f512931df
RasecMH <rasecmh0@gmail.com>
2022-10-01 06:48:04 -0300

    add users table migration

--- src/migrations/20221001093242-create-users.js
+++ src/migrations/20221001093242-create-users.js
@@ -2,20 +2,39 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable(
+      'users',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        displayName: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        password: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        image: {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+      },
+      {
+        underscored: true,
+      }
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.dropTable('users');
+  },
 };
