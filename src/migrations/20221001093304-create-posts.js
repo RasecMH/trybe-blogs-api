fcdf01f33dd6bdeda3165f6a13b4d14b9714cc63
RasecMH <rasecmh0@gmail.com>
2022-10-01 07:01:40 -0300

    add blog_posts and posts_categories migrations

--- src/migrations/20221001093304-create-posts.js
+++ src/migrations/20221001093304-create-posts.js
@@ -2,20 +2,46 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable(
+      'blog_posts',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        title: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        content: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          references: { model: 'users', key: 'id' },
+          allowNull: false,
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+        published: {
+          type: Sequelize.DATETIME,
+          allowNull: false,
+        },
+        update: {
+          type: Sequelize.DATETIME,
+          allowNull: false,
+        },
+      },
+      {
+        underscored: true,
+      }
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.dropTable('blog_posts');
+  },
 };
--- src/migrations/20221001093324-create-posts-categories.js
+++ src/migrations/20221001093324-create-posts-categories.js
@@ -2,20 +2,34 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable(
+      'posts_categories',
+      {
+        postId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: { model: 'blog_posts', key: 'id' },
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE',
+          primaryKey: true,
+        },
+        categoryId: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          references: { model: 'blog_posts', key: 'id' },
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE',
+          primaryKey: true,
+        },
+      },
+      {
+        underscored: true,
+        timestamps: false,
+      }
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.dropTable('posts_categories');
+  },
 };
