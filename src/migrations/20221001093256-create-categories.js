ff223cb437c624acf302634e8cdef4dff3f71173
RasecMH <rasecmh0@gmail.com>
2022-10-01 06:51:31 -0300

    add categories table migration

--- src/migrations/20221001093256-create-categories.js
+++ src/migrations/20221001093256-create-categories.js
@@ -2,20 +2,27 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable(
+      'categories',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        underscored: true,
+      }
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.dropTable('categories');
+  },
 };
