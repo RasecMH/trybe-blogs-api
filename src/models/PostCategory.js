74e57005995146462c2861a54a052bee6a8d083f
RasecMH <rasecmh0@gmail.com>
2022-10-01 08:29:50 -0300

    add category model

---
+++ src/models/PostCategory.js
@@ -0,0 +1,46 @@
+module.exports = (Sequelize, DataTypes) => {
+  const PostCategory = Sequelize.define(
+    'PostCategory',
+    {
+      postId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'blog_posts', key: 'id' },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        primaryKey: true,
+      },
+      categoryId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'categories', key: 'id' },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        primaryKey: true,
+      },
+    },
+    {
+      tableName: 'post_categories',
+      underscored: true,
+      timestamps: false,
+    }
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blog_posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
