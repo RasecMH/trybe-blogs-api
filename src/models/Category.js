a3609650982c2215c3756bce860681ca3e0d7403
RasecMH <rasecmh0@gmail.com>
2022-10-01 08:35:10 -0300

    add category model

---
+++ src/models/Category.js
@@ -0,0 +1,32 @@
+module.exports = (Sequelize, DataTypes) => {
+  const Category = Sequelize.define(
+    'Category',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'categories',
+      underscored: true,
+      timestamps: false,
+    }
+  );
+
+  Category.associate = (models) => {
+    Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      foreignKey: 'categoryId',
+      through: 'PostCategory',
+    });
+  };
+
+  return Category;
+};
