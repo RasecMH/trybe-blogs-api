12ab852bac0ad88c38e2dc4af89207d463233921
RasecMH <rasecmh0@gmail.com>
2022-10-01 08:38:17 -0300

    add BlogPost model

---
+++ src/models/BlogPost.js
@@ -0,0 +1,50 @@
+module.exports = (Sequelize, DataTypes) => {
+  const BlogPost = Sequelize.define(
+    'BlogPost',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: { model: 'users', key: 'id' },
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      published: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'BlogPosts',
+      underscored: true,
+      timestamps: false,
+    }
+  );
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      as: 'user',
+      foreignKey: 'userId',
+    });
+  };
+
+  return BlogPost;
+};
