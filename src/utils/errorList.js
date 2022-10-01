85b58097fc2e66bff87354984c515dfffbcb8548
RasecMH <rasecmh0@gmail.com>
2022-10-01 14:06:11 -0300

    add error list

--- src/utils/errorList.js
+++ src/utils/errorList.js
@@ -1,6 +1,46 @@
 module.exports = {
-  NOTFOUND: {
+  EMPITYFIELDS: {
+    status: 400,
+    message: 'Some required fields are missing',
+  },
+  FIELDNOTFOUND: {
+    status: 400,
+    message: 'Invalid fields',
+  },
+  INVALIDDISPLAYNAMELENGTH: {
+    status: 400,
+    message: '"displayName" length must be at least 8 characters long',
+  },
+  INVALIDEMAILFIELD: {
+    status: 400,
+    message: '"email" must be a valid email',
+  },
+  INVALIDPASSWORDLENGTH: {
+    status: 400,
+    message: '"password" length must be at least 6 characters long',
+  },
+  EMAILALREADYINDATABASE: {
+    status: 409,
+    message: 'User already registered',
+  },
+  USERNOTFOUND: {
     status: 404,
-    message: 'file not found',
+    message: 'User does not exist',
+  },
+  EMPITYNAMEFIELD: {
+    status: 400,
+    message: '"name" is required',
+  },
+  CATEGORYNOTFOUND: {
+    status: 400,
+    message: '"categoryIds" not found',
+  },
+  POSTNOTFOUND: {
+    status: 404,
+    message: 'Post does not exist',
+  },
+  UNAUTHORIZEDUSER: {
+    status: 401,
+    message: 'Unauthorized user',
   },
 };
