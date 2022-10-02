571eb47d7389c448d981e64c23b39375a95a6b1b
RasecMH <rasecmh0@gmail.com>
2022-10-02 10:45:41 -0300

    force evaluator

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -2,19 +2,16 @@ const { BlogPost, PostCategory, sequelize } = require('../models');
 
 const create = async ({ title, content, categoryIds, id }) => {
   console.log('Service: ', categoryIds);
-  const result = await sequelize.transaction(async (t) => {
+  return sequelize.transaction(async (t) => {
     const newPost = await BlogPost.create(
       { title, content, userId: id, published: Date.now(), updated: Date.now() },
       { transaction: t },
     );
-    console.log('Transacition: ', newPost.dataValues.id);
 
     await Promise.all(categoryIds.map(async (categoryId) => 
     PostCategory.create({ postId: newPost.dataValues.id, categoryId }, { transaction: t })));
-
     return newPost;
   });
-  return result;
 };
 
 module.exports = { create };
