160c3246c958a5656387a7199167b03134bbb481
RasecMH <rasecmh0@gmail.com>
2023-02-05 06:11:10 -0300

    fix index

--- api/index.js
+++ api/index.js
@@ -5,8 +5,20 @@ const loginRoute = require('../src/routers/login.router');
 const userRoute = require('../src/routers/user.router');
 const postRoute = require('../src/routers/post.router');
 // ...
+const app = require('express')();
+const { v4 } = require('uuid');
 
-const app = express();
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 
 app.use(express.json());
 
